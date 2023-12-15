52d7ed58b7f07bcc6b6790d3abcaadd30d8ccb58
timmashkov <bodomizer#yandex.ru>
2023-12-15 12:43:06 +0300

    added some minor changes

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,6 +1,7 @@
+import secrets
 from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 router = APIRouter(
@@ -12,7 +13,27 @@ security = HTTPBasic()
 
 
 @router.get("/basic_auth")
-async def get_credentials(credentials: Annotated[HTTPBasicCredentials, security]):
+def get_credentials(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
     return {"messages": "Hi",
             "name": credentials.username,
             "password": credentials.password}
+
+
+database = {"timur": 'hello', "user": 'user'}
+
+
+def get_user(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    unauth_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                     detail="Wrong username or password",
+                                     headers={"WWW-Authenticate": "Basic"})
+    correct_pass = database.get(credentials.username)
+    if correct_pass is None:
+        raise unauth_exception
+    if not secrets.compare_digest(credentials.password.encode("utf-8"), correct_pass.encode("utf-8")):
+        raise unauth_exception
+    return credentials.username
+
+
+@router.get("/auth_user")
+def get_credentials_username(auth_username: str = Depends(get_user)):
+    return {"messages": f"Hi {auth_username}"}
